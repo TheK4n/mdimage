f9e7bddc542754200dc846c3a5bc38145f2c60b0
thek4n <thek4n@yandex.ru>
2024-02-27 13:21:39 +0300

    refactor

--- plugin/mdimage.lua
+++ plugin/mdimage.lua
@@ -1,27 +1,38 @@
 
-function ExecCurl(input)
-    local cnf = require("mdimage").options
+local function copyFile(filename, newfilename)
+    vim.fn.system({'cp', filename, newfilename})
+end
 
-    local note_prefix = cnf.img_path
+local function formatFilename(filename)
+    return os.time() .. "_" .. vim.fs.basename(vim.fs.normalize(filename))
+end
 
-    local new_imagename =  os.time() .. "_" .. vim.fs.basename(vim.fs.normalize(input.args))
+local function createLink(path, name)
+    local new_imagepath = vim.fs.normalize(path)
+    return "![" .. name .. "](" .. new_imagepath .. ")"
+end
 
+local function pasteUnderCursor(text)
+    local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+    vim.api.nvim_buf_set_text(0, row - 1, col, row - 1, col, { text })
+end
+
+function CopyImageAndPasteLink(input)
+    local note_prefix = require("mdimage").options.img_path
+
+    local new_imagename = formatFilename(input.args)
     local new_imagepath = note_prefix .. "/" .. new_imagename
 
-    new_imagepath = vim.fs.normalize(new_imagepath)
+    copyFile(input.args, new_imagepath)
 
-    vim.fn.system({'cp', input.args, new_imagepath})
-
-    local md_tag = "![" .. new_imagename .. "](" .. new_imagepath .. ")"
-
-    local row, col = unpack(vim.api.nvim_win_get_cursor(0))
-    vim.api.nvim_buf_set_text(0, row - 1, col, row - 1, col, { md_tag })
+    local md_tag = createLink(new_imagepath, new_imagename)
+    pasteUnderCursor(md_tag)
 end
 
 
 vim.api.nvim_create_autocmd({"FileType", "BufEnter"}, {
     pattern = "markdown",
     callback = function()
-        vim.api.nvim_create_user_command("PasteImage", ExecCurl, { nargs = 1, complete = 'file' })
+        vim.api.nvim_create_user_command("PasteImage", CopyImageAndPasteLink, { nargs = 1, complete = 'file' })
     end
 })
